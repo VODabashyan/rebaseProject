6a65414212f8b409e28dc8ddbcb05e30c54a0050
ArmenAtalyan <atalyan3@gmail.com>
2019-12-15 23:49:29 +0400

    without coments

--- src/Person.java
+++ src/Person.java
@@ -1,16 +1,10 @@
 import java.io.Serializable;
 
-//to make a class serializable we need to implement the Serializable interface.
 public class Person implements Serializable{
     /**
      *
      */
-    private static final long serialVersionUID = 4801633306273802062L; //we get a warning when not adding a serialVersionUID we "add generated serial version UID" to get rid of the warning. In this case a serialVersionUID with a random number
-    //"4801633306273802062L" is added. So, in order to deserialize(read from a file) the objects that we've previously serialized (written to a file)
-    //and if serialVersionUID was defined when serializing the objects, then serialVersionUID must be the same when deserializing that objects. In other words, the serialVersionUID
-    //of the class Person must be the same when serializing and deserializing the objects of Person class. So, this is simply a way of checking that we are reading with the exact same
-    //class that we wrote with. So, the main use of serialVersionUID is to stop the newer versions of our class/program to read the objects serialized with very old versions of that
-    //class/program by simply changing the serialVersionUID.
+    private static final long serialVersionUID = 4801633306273802062L;
     private int id;
     private String name;
     private String email;
@@ -25,5 +19,4 @@ public class Person implements Serializable{
     public String toString() {
         return "Person [id=" + id + ", name=" + name + "]";
     }
-
 }
--- src/ReadObjects.java
+++ src/ReadObjects.java
@@ -4,31 +4,24 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 
 public class ReadObjects {
+
     public static void main(String[] args) {
         System.out.println("Reading objects...");
 
-        try(FileInputStream fi = new FileInputStream("people.bin")) {//we pass the name of the file that we want to read from
-
+        try(FileInputStream fi = new FileInputStream("people.bin")) {
             ObjectInputStream os = new ObjectInputStream(fi);
-
-            Person person1 = (Person)os.readObject(); //as readObject() returns an Object we need to cast it to Person type of object.
+            Person person1 = (Person)os.readObject();
             Person person2 = (Person)os.readObject();
 
             os.close();
-
-            //to make sure that we've read the objects. By the way, we've read them in the same order as in case of writing.
             System.out.println(person1);
             System.out.println(person2);
 
         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
-        } catch (IOException e) {//will be thrown if we can't read from "people.bin" file.
-            // TODO Auto-generated catch block
+        } catch (IOException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {//this will be thrown if readObject() reads an object of a class that doesn't exist in this program
-            //i.e we will get this exception if we try to read an object of a class that isn't defined in our program
-            // TODO Auto-generated catch block
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
