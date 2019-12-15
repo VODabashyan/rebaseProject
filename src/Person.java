181150215daa9d8f4e071f460142f0c2fac2ea5a
ArmenAtalyan <atalyan3@gmail.com>
2019-12-15 23:38:11 +0400

    new fields in Person

--- src/Person.java
+++ src/Person.java
@@ -13,10 +13,12 @@ public class Person implements Serializable{
     //class/program by simply changing the serialVersionUID.
     private int id;
     private String name;
+    private String email;
 
-    public Person(int id, String name) {
+    public Person(int id, String name, String email) {
         this.id = id;
         this.name = name;
+        this.email = email;
     }
 
     @Override
